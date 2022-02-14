58b8a1747680a59010ca59a270882757edb1bda8
Ridhwanul Haque <rhemon19@gmail.com>
2022-02-14 19:31:08 +0000

    Separate train method

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 import torch.optim as optim
 import shutil
 
+from train import train
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-tp", "--train-path", help="Path to training json file")
 
@@ -43,32 +45,8 @@ if __name__ == '__main__':
         checkpoint_folder = model_desc / Path(time_stamp)
         checkpoint_folder.mkdir(parents=True, exist_ok=True)
         shutil.copy(Path(args.train_path), checkpoint_folder)
-        ep = 1
-        cpt_track_step = 0
-        for epoch in range(total_epoch):
-            for batch_idx, (event_x, event_time_x, lens, result) in enumerate(train_loader):
-                event_x = event_x.type(torch.LongTensor).to(device)
-                event_time_x = event_time_x.type(torch.LongTensor).to(device)
-                # lens = lens.to(device)
-                result = result.to(device)
-        #         result = result.type(torch.LongTensor)
-                scores = model(event_x, event_time_x, lens).squeeze(1)
-                loss = loss_fn(scores, result)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                checkpoint_name = checkpoint_folder / Path(str(epoch)+"_"+str(batch_idx)+".pt")
-                cpt_track_step += 1
-                if cpt_track_step % cfg.CPT_EVERY == 0:
-                    torch.save({
-                        'epoch': epoch,
-                        'batch_idx': batch_idx,
-                        'model_state_dict': model.state_dict(),
-                        'optimizer_state_dict': optimizer.state_dict(),
-                        'loss': loss,
-                        }, checkpoint_name)
-                    print(f'epoch: {epoch + 1} step: {batch_idx + 1}/{len(train_loader)} loss: {loss}')
-                    cpt_track_step = 0
-            ep += 1
+        
+        train(total_epoch, model, loss_fn, optimizer, checkpoint_folder, device, train_loader)
+
     else:
         print("No training configuration provided")
---
+++ train.py
@@ -0,0 +1,32 @@
+import torch
+from pathlib import Path
+
+
+def train(total_epoch, model, loss_fn, optimizer, checkpoint_folder, device, train_loader):
+    ep = 1
+    cpt_track_step = 0
+    for epoch in range(total_epoch):
+        for batch_idx, (event_x, event_time_x, lens, result) in enumerate(train_loader):
+            event_x = event_x.type(torch.LongTensor).to(device)
+            event_time_x = event_time_x.type(torch.LongTensor).to(device)
+            # lens = lens.to(device)
+            result = result.to(device)
+    #         result = result.type(torch.LongTensor)
+            scores = model(event_x, event_time_x, lens).squeeze(1)
+            loss = loss_fn(scores, result)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            checkpoint_name = checkpoint_folder / Path(str(epoch)+"_"+str(batch_idx)+".pt")
+            cpt_track_step += 1
+            if cpt_track_step % cfg.CPT_EVERY == 0:
+                torch.save({
+                    'epoch': epoch,
+                    'batch_idx': batch_idx,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss,
+                    }, checkpoint_name)
+                print(f'epoch: {epoch + 1} step: {batch_idx + 1}/{len(train_loader)} loss: {loss}')
+                cpt_track_step = 0
+        ep += 1
