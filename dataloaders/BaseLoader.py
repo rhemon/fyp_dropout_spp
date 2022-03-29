a0cd0633f9f0c5b6e4741a59dc3310ec0e352324
Ridhwanul Haque <rhemon19@gmail.com>
2022-03-29 16:29:34 +0100

    Comment and document BaseLoader

--- dataloaders/BaseLoader.py
+++ dataloaders/BaseLoader.py
@@ -1,25 +1,28 @@
-from xml.dom.minidom import Attr
 import numpy as np
-import pandas as pd
-import datetime
-import torch
-
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle 
+import torch
 
 
 class BaseLoader:
     """
-    Class to laod KEATS dataset as OHV input, although actualy output is essentially indexes of the onehot vector position
-    as data will be used in a GritNet model that uses Embedding layer.
+    BaseLoader class that sets some of the root functionalties
+    that are common throughout all the loaders.
 
-    Takes in columns type and output type from cfg
-    Columns type to specify which fields to use. Output type Binary or multivariative (support not added yet) 
-
+    Specifically the load_dataset method that performs the split
+    for both class distribution and train-test. 
     """    
 
     def __init__(self, cfg, checkpoint_folder=None):
+        """
+        BaseLoader Constructor. Sets up load method, class sizes if specified in 
+        configuration else set to None.
+        
+        And sets checkpoint folder to passed value.
 
+        @param cfg               : SimpleNamespace object created from json object 
+        @param checkpoint_folder : None or Path object specifying path of foldre where checkpoints are saved.
+        """
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         
         self.load_method = cfg.LOAD_METHOD
@@ -28,14 +31,27 @@ class BaseLoader:
         except AttributeError:
             self.class_sizes = None
         
-        
         self.checkpoint_folder = checkpoint_folder
 
-
     def create_dataset(self):
+        """
+        Not implemented in Base class. Expected to be implemented in loaders
+        that are used when training models.
+        """
         pass
     
     def split_class_sets(self, all_class_sets, test_split_ratio=0.2):
+        """
+        Split data and target of each class into train and test set.
+        By default, split ration is 80-20 for train-test.
+
+        @param all_class_sets     : A list of list of tensors (List<List<Tensor>>) where each 
+                                    List<Tensor> is tensors of a single class.
+        @param test_split_ration  : Float, default to 0.2. Percentage of each class that goes
+                                    into test set.
+
+        @return Tuple<List<List<Tensor>>> where the tuple has two List<List<Tensor>> (train, test).
+        """
         train_sets = []
         test_sets = []
         for class_sets in all_class_sets:
@@ -50,6 +66,17 @@ class BaseLoader:
         return train_sets, test_sets
             
     def oversample(self, all_class_sets, max_sample_per_class):
+        """
+        Oversample data of each class to match max_sample_per_class
+
+        @param all_class_sets       : A list of list of tensors (List<List<Tensor>>) where each 
+                                      List<Tensor> is tensors of a single class.
+        @param max_sample_per_class : A int value, where number of examples per class is
+                                      sampled to match it.
+
+        @return List<List<Tensor>> where each Tensor is oversampled to have 
+                max_sample_per_class examples.
+        """
         for i in range(len(all_class_sets)):
             num_to_duplicate = max_sample_per_class - all_class_sets[i][0].shape[0]
             if num_to_duplicate > 0:
@@ -59,6 +86,18 @@ class BaseLoader:
         return all_class_sets
     
     def combine_classes(self, all_class_sets, controlled=False):
+        """
+        Combine classes List<List<Tensor>> into single List<Tensor>.
+        If controlled set to true then class sizes to sample number of examples
+        specified in the configuration.
+
+        @param all_class_sets : A list of list of tensors (List<List<Tensor>>) where each 
+                                List<Tensor> is tensors of a single class.
+        @param controlled     : Boolean value. Whether to sample according to specified
+                                split or not.
+        
+        @return List<Tensor> combining all classes into single Tensor (normally just x, y).
+        """
         dataset = [[] for i in range(len(all_class_sets[0]))]
         
         for i, class_set in enumerate(all_class_sets):
@@ -77,6 +116,21 @@ class BaseLoader:
         return dataset
 
     def load_dataset(self, test_split_ratio=0.2):
+        """
+        Load dataset and oversample and split accordingly if needed. If oversample set by 
+        configaration then examples in train set are oversampled to have equal 
+        distribution.Also won trian set if class split is specified then it is used
+        to sample examples per class accordingly. 
+
+        Test set set is kept same. Random state is set to 0so for all datasets 
+        the split will be always same. Making comparision of results fair.
+
+        @param test_split_ration : Floating value (default 0.2) specifying how much of
+                                   full dataset goes into test set.
+        
+        @return Tuple<Tuple<Tensor>> where normally expected to be: 
+                (X_train, y_train), (X_test, y_test)
+        """
         datasets = self.create_dataset() ## [x, y]
         classes = list(datasets[-1].unique())
         
@@ -104,6 +158,5 @@ class BaseLoader:
 
         print("Train:", dataset_train[0].shape[0])
         print("Test:", dataset_test[0].shape[0])
-        print("Max sample per class:", max_sample_per_class)
 
         return dataset_train, dataset_test
